d2db12469aefc741068ee31f0dc52fb00c3220e9
kielson <kielek125@o2.pl>
2017-10-18 22:19:30 +0200

    Dorobienie metod validacyjnych

--- app/src/main/java/com/messanger/firebase/malavero/abyssgates/ForgotLoginRequest.java
+++ app/src/main/java/com/messanger/firebase/malavero/abyssgates/ForgotLoginRequest.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
-
 import com.messanger.firebase.malavero.abyssgates.FINAL.StaticMethods;
 
 public class ForgotLoginRequest extends Activity {
@@ -19,44 +18,79 @@ public class ForgotLoginRequest extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_login_request);
 
-        forgotEmail = (EditText)findViewById(R.id.forgotEmailEditText);
-        forgotPassword = (EditText)findViewById(R.id.forgotPasswordEditText);
-        passwordForgotImageView = (ImageView)findViewById(R.id.forgotPasswordTextView);
+        forgotEmail = (EditText) findViewById(R.id.forgotEmailEditText);
+        forgotPassword = (EditText) findViewById(R.id.forgotPasswordEditText);
+        passwordForgotImageView = (ImageView) findViewById(R.id.forgotPasswordTextView);
         mode = getIntent().getIntExtra("MODE", 0);
         setDisplay();
     }
-    public void forgotLoginRequest(View view){
+
+    public void forgotLoginRequest(View view) {
         try {
-
-            if (mode == 1) { //TODO przypomnienie hasła
-
-            } else if (mode == 2) { //TODO przypomnienie loginu
-
-            }
-            //TODO jezeli email i haslo sie zgadzaja to wyslij login na maila
             String email = forgotEmail.getText().toString();
-            if(!StaticMethods.emailValid(email)){
+            String password = forgotPassword.getText().toString();
+            if (email.equals("")) {
+                StaticMethods.toastMessage("Enter your email", getApplicationContext());
+                return;
+            }
+            if (password.equals("")) {
+                StaticMethods.toastMessage("Enter your password", getApplicationContext());
+                return;
+            }
+            if (mode == 1) { //przypomnienie hasła
+                recoverPassword(email);
+            } else if (mode == 2) { //przypomnienie loginu
+                recoverLogin(email, password);
+            }
+            if (!StaticMethods.emailValid(email)) {
                 StaticMethods.toastMessage("Invalid email", getApplicationContext());
                 return;
             }
-            String password = forgotPassword.getText().toString();
-            //TODO połącz do bazy i sprawdz czy istnieją
-            if (false && false) {
-                StaticMethods.toastMessage("We sent to you your login", getApplicationContext());
-                //TODO wyslij na podany adres przypomnienie z loginem
-            } else {
-                StaticMethods.toastMessage("Your email address or password is incorrect", getApplicationContext());
-            }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             StaticMethods.toastException(e, getApplicationContext());
         }
     }
 
-    private void setDisplay(){
-        if(mode == 1){
+    private void setDisplay() {
+        if (mode == 1) {
             passwordForgotImageView.setVisibility(View.INVISIBLE);
             forgotPassword.setVisibility(View.INVISIBLE);
         }
+    }
+
+    private void recoverPassword(String email) {
+
+        if (checkIfEmailExist(email)) { //- proponuje zrobic klase do obslugi połączeń zamiast pisać 15 razy tą samą metode
+            sendNewPasswordToUserEmail(email);
+        } else {
+            StaticMethods.toastMessage("Email does not exist", getApplicationContext());
+        }
+    }
+
+    private void recoverLogin(String email, String password) {
+        if (checkIfEmailExist(password)) { // - jak wyżej
+            if (checkPasswordIfExist(email)) {
+                sendUserLoginToUserEmail(email);
+            } else {
+                StaticMethods.toastMessage("Login does not exist", getApplicationContext());
+            }
+        } else {
+            StaticMethods.toastMessage("Email does not exist", getApplicationContext());
+        }
+    }
+
+    private boolean checkIfEmailExist(String email) {
+        //TODO
+        return true;
+    }
+    private boolean checkPasswordIfExist(String password) {
+        //TODO
+        return true;
+    }
+    private void sendNewPasswordToUserEmail(String email){
+        //TODO
+    }
+    private void sendUserLoginToUserEmail(String email){
+        //TODO
     }
 }
