f14d3a47fab25bcf7f80da1ef884225789cf058a
kielson <kielek125@o2.pl>
2017-10-21 12:08:08 +0200

    Poprawki

--- app/src/main/java/com/messanger/firebase/malavero/abyssgates/ChoosePlanetActivity.java
+++ app/src/main/java/com/messanger/firebase/malavero/abyssgates/ChoosePlanetActivity.java
@@ -2,27 +2,35 @@ package com.messanger.firebase.malavero.abyssgates;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.messanger.firebase.malavero.abyssgates.FINAL.StaticMethods;
 
 public class ChoosePlanetActivity extends Activity {
 
     private int yourPlanet = 0;
     private ImageView choosePlanet;
+    private TextView infoPlanetDesert, infoPlanetIce, infoPlanetJungle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_planet);
 
         choosePlanet = (ImageView)findViewById(R.id.choosePlanetImageView);
+        infoPlanetDesert = (TextView)findViewById(R.id.infoPlanetDesertTextView);
+        infoPlanetIce = (TextView)findViewById(R.id.infoPlanetIceTextView);
+        infoPlanetJungle = (TextView)findViewById(R.id.infoPlanetJungleTextView);
     }
 
     public void chooseYourFirstPlanet(View view) {
         //TODO powiedz bazie jaka gracz wybrał planete
         //TODO wywołaj metodę renderującą pozycje gracza w układzie
         Intent afterChoosePlanet = new Intent(this, PlanetActivity.class);
-        afterChoosePlanet.getIntExtra("ChoosenPlanet", yourPlanet);
+        afterChoosePlanet.putExtra("ChoosenPlanet", yourPlanet);
         startActivity(afterChoosePlanet);
         finish();
     }
@@ -31,12 +39,21 @@ public class ChoosePlanetActivity extends Activity {
         switch (view.getId()) {
             case R.id.selectPlanetJungleImageView:
                 yourPlanet = 1;
-                break;
-            case R.id.selectPlanetIceImageView:
-                yourPlanet = 2;
+                infoPlanetJungle.setBackgroundColor(Color.parseColor("#4066cd00"));
+                infoPlanetIce.setBackgroundResource(0);
+                infoPlanetDesert.setBackgroundColor(0);
                 break;
             case R.id.selectPlanetDesertImageView:
+                yourPlanet = 2;
+                infoPlanetDesert.setBackgroundColor(Color.parseColor("#4066cd00"));
+                infoPlanetJungle.setBackgroundResource(0);
+                infoPlanetIce.setBackgroundResource(0);
+                break;
+            case R.id.selectPlanetIceImageView:
                 yourPlanet = 3;
+                infoPlanetIce.setBackgroundColor(Color.parseColor("#4066cd00"));
+                infoPlanetJungle.setBackgroundResource(0);
+                infoPlanetDesert.setBackgroundResource(0);
                 break;
         }
         if(yourPlanet != 0){
--- app/src/main/java/com/messanger/firebase/malavero/abyssgates/PlanetActivity.java
+++ app/src/main/java/com/messanger/firebase/malavero/abyssgates/PlanetActivity.java
@@ -4,61 +4,70 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapRegionDecoder;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Point;
+import android.graphics.PointF;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
+import android.util.Log;
 import android.view.Display;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
 import com.messanger.firebase.malavero.abyssgates.FINAL.StaticMethods;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class PlanetActivity extends Activity {
+public class PlanetActivity extends Activity implements View.OnTouchListener {
 
-    ImageView backgroundJungle;
+    ImageView backgroundMap;
     private int choosenPlanet = 0;
     public int WIDTH = 0;
     public int HEIGHT = 0;
-    public int DISPLAYWIDTH = 0;
-    public int DISPLAYHEIGHT = 0;
+//    public int DISPLAYWIDTH = 0;
+//    public int DISPLAYHEIGHT = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_planet);
+
 
         //TODO pobierz planete z bazy, jesli zwroci 0 to znaczy ze nie istnieje wiec pobierz z wczesniejszego intentu
         choosenPlanet = getIntent().getIntExtra("ChoosenPlanet", 0);
+        backgroundMap = (ImageView) findViewById(R.id.backgroundMap);
 
-        new SwipeDetector(getWindow().getDecorView().findViewById(R.id.content)).setOnSwipeListener(new SwipeDetector.onSwipeEvent() {
-            @Override
-            public void SwipeEventDetected(View v, SwipeDetector.SwipeTypeEnum SwipeType) {
-                if(SwipeType == SwipeDetector.SwipeTypeEnum.LEFT_TO_RIGHT)
-                    Toast.makeText(getApplicationContext(), "igor to pala", Toast.LENGTH_SHORT).show(); //TODO
-            }
-        });
-        try {
-            if (choosenPlanet == 1)
-                backgroundJungle = (ImageView) findViewById(R.id.backgroundJungle);
-            else if (choosenPlanet == 2)
-                backgroundJungle = (ImageView) findViewById(R.id.backgroundJungle); //TODO
-            else if (choosenPlanet == 3)
-                backgroundJungle = (ImageView) findViewById(R.id.backgroundJungle); //TODO
-        }
-        catch(Exception e){
-            StaticMethods.toastException(e, this);
-        }
+        backgroundMap.setOnTouchListener(this);
+
 
         InputStream is = null;
         try {
-            Drawable drawable = getResources().getDrawable(R.drawable.junglemap);
+            Drawable drawable;
+            switch (choosenPlanet) {
+                case 1:
+                    drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.junglemap, null);
+                    break;
+                case 2:
+                    drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.desertmap, null);
+                    break;
+                case 3:
+                    drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.icemap, null);
+                    break;
+                default:
+                    drawable = null;
+                    break;
+            }
             BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
             Bitmap bitmap = bitmapDrawable.getBitmap();
             WIDTH = bitmap.getWidth();
@@ -70,26 +79,147 @@ public class PlanetActivity extends Activity {
             StaticMethods.toastException(ex, this);
         }
 
-        try {
-            Display display = getWindowManager().getDefaultDisplay();
-            Point size = new Point();
-            display.getSize(size);
-            DISPLAYWIDTH = size.x;
-            DISPLAYHEIGHT = size.y;
-        } catch (Exception ex) {
-            StaticMethods.toastException(ex, this);
-        }
+//        try {
+//            Display display = getWindowManager().getDefaultDisplay();
+//            Point size = new Point();
+//            display.getSize(size);
+//            DISPLAYWIDTH = size.x;
+//            DISPLAYHEIGHT = size.y;
+//        } catch (Exception ex) {
+//            StaticMethods.toastException(ex, this);
+//        }
         try {
             if (is != null) {
                 BitmapRegionDecoder decoder = BitmapRegionDecoder.newInstance(is, false);
                 BitmapFactory.Options options = new BitmapFactory.Options();
                 options.inPreferredConfig = Bitmap.Config.RGB_565;
-                Bitmap partOfBitmap = decoder.decodeRegion(new Rect(0, 0, DISPLAYWIDTH, DISPLAYHEIGHT), options);
-                backgroundJungle.setImageBitmap(partOfBitmap);
-                Toast.makeText(getApplicationContext(), DISPLAYWIDTH + " " + DISPLAYHEIGHT, Toast.LENGTH_LONG).show();
+                Bitmap partOfBitmap = decoder.decodeRegion(new Rect(0, 0, WIDTH, HEIGHT), options);
+                backgroundMap.setImageBitmap(partOfBitmap);
+                Toast.makeText(getApplicationContext(), WIDTH + " " + HEIGHT, Toast.LENGTH_LONG).show();
             }
         } catch (IOException ex) {
             StaticMethods.toastException(ex, this);
         }
+    }
+
+
+    private static final String TAG = "Touch";
+    @SuppressWarnings("unused")
+    private static final float MIN_ZOOM = 10f, MAX_ZOOM = 100f;
+
+    Matrix matrix = new Matrix();
+    Matrix savedMatrix = new Matrix();
+
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    int mode = NONE;
+
+    PointF start = new PointF();
+    PointF mid = new PointF();
+    float oldDist = 1f;
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        ImageView view = (ImageView) v;
+        view.setScaleType(ImageView.ScaleType.MATRIX);
+        float scale;
+        //dumpEvent(event);
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:   // first finger down only
+                savedMatrix.set(matrix);
+                start.set(event.getX(), event.getY());
+                Log.d(TAG, "mode=DRAG"); // write to LogCat
+                mode = DRAG;
+                break;
+
+            case MotionEvent.ACTION_UP: // first finger lifted
+
+            case MotionEvent.ACTION_POINTER_UP: // second finger lifted
+
+                mode = NONE;
+                Log.d(TAG, "mode=NONE");
+                break;
+
+            case MotionEvent.ACTION_POINTER_DOWN: // first and second finger down
+
+                oldDist = spacing(event);
+                Log.d(TAG, "oldDist=" + oldDist);
+                if (oldDist > 5f) {
+                    savedMatrix.set(matrix);
+                    midPoint(mid, event);
+                    mode = ZOOM;
+                    Log.d(TAG, "mode=ZOOM");
+                }
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+
+                if (mode == DRAG) {
+                    matrix.set(savedMatrix);
+                    matrix.postTranslate(event.getX() - start.x, event.getY() - start.y); // create the transformation in the matrix  of points
+                } else if (mode == ZOOM) {
+
+                    // pinch zooming
+                    float newDist = spacing(event);
+                    Log.d(TAG, "newDist=" + newDist);
+                    if (newDist > 5f) {
+                        matrix.set(savedMatrix);
+                        scale = newDist / oldDist; // setting the scaling of the
+                        // matrix...if scale > 1 means
+                        // zoom in...if scale < 1 means
+                        // zoom out
+                        if (scale > 0.75 || scale < 1.75)
+                            matrix.postScale(scale, scale, mid.x, mid.y);
+
+                    }
+                }
+                break;
+        }
+
+        view.setImageMatrix(matrix); // display the transformation on screen
+
+        return true; // indicate event was handled
+    }
+
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (float) Math.sqrt(x * x + y * y);
+    }
+
+    private void midPoint(PointF point, MotionEvent event) {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
+    }
+
+    private void dumpEvent(MotionEvent event) {
+//        String names[] = { "DOWN", "UP", "MOVE", "CANCEL", "OUTSIDE","POINTER_DOWN", "POINTER_UP", "7?", "8?", "9?" };
+//        StringBuilder sb = new StringBuilder();
+//        int action = event.getAction();
+//        int actionCode = action & MotionEvent.ACTION_MASK;
+//        sb.append("event ACTION_").append(names[actionCode]);
+//
+//        if (actionCode == MotionEvent.ACTION_POINTER_DOWN || actionCode == MotionEvent.ACTION_POINTER_UP)
+//        {
+//            sb.append("(pid ").append(action >> MotionEvent.ACTION_POINTER_ID_SHIFT);
+//            sb.append(")");
+//        }
+//
+//        sb.append("[");
+//        for (int i = 0; i < event.getPointerCount(); i++)
+//        {
+//            sb.append("#").append(i);
+//            sb.append("(pid ").append(event.getPointerId(i));
+//            sb.append(")=").append((int) event.getX(i));
+//            sb.append(",").append((int) event.getY(i));
+//            if (i + 1 < event.getPointerCount())
+//                sb.append(";");
+//        }
+//
+//        sb.append("]");
+//        Log.d("Touch Events ---------", sb.toString());
     }
 }
