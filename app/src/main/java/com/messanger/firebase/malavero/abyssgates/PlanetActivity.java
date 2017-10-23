72409cd16f932335c629c66897dc5d43358731ba
kielson <kielek125@o2.pl>
2017-10-23 17:11:35 +0200

    Poprawki

--- app/src/main/java/com/messanger/firebase/malavero/abyssgates/PlanetActivity.java
+++ app/src/main/java/com/messanger/firebase/malavero/abyssgates/PlanetActivity.java
@@ -5,7 +5,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapRegionDecoder;
 import android.graphics.Matrix;
-import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
@@ -13,20 +12,18 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
 import android.util.Log;
-import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
-
 import com.messanger.firebase.malavero.abyssgates.FINAL.StaticMethods;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import static com.messanger.firebase.malavero.abyssgates.R.id.flagBudiling;
+import static android.R.interpolator.linear;
 
 public class PlanetActivity extends Activity implements View.OnTouchListener {
 
@@ -34,8 +31,7 @@ public class PlanetActivity extends Activity implements View.OnTouchListener {
     private int choosenPlanet = 0;
     public int WIDTH = 0;
     public int HEIGHT = 0;
-//    public int DISPLAYWIDTH = 0;
-//    public int DISPLAYHEIGHT = 0;
+    ZoomLayout zoomLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +43,8 @@ public class PlanetActivity extends Activity implements View.OnTouchListener {
         choosenPlanet = getIntent().getIntExtra("ChoosenPlanet", 0);
         backgroundMap = (ImageView) findViewById(R.id.backgroundMap);
 
-        backgroundMap.setOnTouchListener(this);
+        zoomLayout = (ZoomLayout)findViewById(R.id.zoomLayout);
+        zoomLayout.setOnTouchListener(PlanetActivity.this);
 
         InputStream is = null;
         try {
@@ -77,15 +74,6 @@ public class PlanetActivity extends Activity implements View.OnTouchListener {
             StaticMethods.toastException(ex, this);
         }
 
-//        try {
-//            Display display = getWindowManager().getDefaultDisplay();
-//            Point size = new Point();
-//            display.getSize(size);
-//            DISPLAYWIDTH = size.x;
-//            DISPLAYHEIGHT = size.y;
-//        } catch (Exception ex) {
-//            StaticMethods.toastException(ex, this);
-//        }
         try {
             if (is != null) {
                 BitmapRegionDecoder decoder = BitmapRegionDecoder.newInstance(is, false);
@@ -99,124 +87,8 @@ public class PlanetActivity extends Activity implements View.OnTouchListener {
             StaticMethods.toastException(ex, this);
         }
     }
-
-
-    private static final String TAG = "Touch";
-    @SuppressWarnings("unused")
-    private static final float MIN_ZOOM = 10f, MAX_ZOOM = 100f;
-
-    Matrix matrix = new Matrix();
-    Matrix savedMatrix = new Matrix();
-
-    static final int NONE = 0;
-    static final int DRAG = 1;
-    static final int ZOOM = 2;
-    int mode = NONE;
-
-    PointF start = new PointF();
-    PointF mid = new PointF();
-    float oldDist = 1f;
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        ImageView view = (ImageView) v;
-        view.setScaleType(ImageView.ScaleType.MATRIX);
-        float scale;
-        //dumpEvent(event);
-
-        switch (event.getAction() & MotionEvent.ACTION_MASK) {
-            case MotionEvent.ACTION_DOWN:   // first finger down only
-                savedMatrix.set(matrix);
-                start.set(event.getX(), event.getY());
-                Log.d(TAG, "mode=DRAG"); // write to LogCat
-                mode = DRAG;
-                break;
-
-            case MotionEvent.ACTION_UP: // first finger lifted
-
-            case MotionEvent.ACTION_POINTER_UP: // second finger lifted
-
-                mode = NONE;
-                Log.d(TAG, "mode=NONE");
-                break;
-
-            case MotionEvent.ACTION_POINTER_DOWN: // first and second finger down
-
-                oldDist = spacing(event);
-                Log.d(TAG, "oldDist=" + oldDist);
-                if (oldDist > 5f) {
-                    savedMatrix.set(matrix);
-                    midPoint(mid, event);
-                    mode = ZOOM;
-                    Log.d(TAG, "mode=ZOOM");
-                }
-                break;
-
-            case MotionEvent.ACTION_MOVE:
-
-                if (mode == DRAG) {
-                    matrix.set(savedMatrix);
-                    matrix.postTranslate(event.getX() - start.x, event.getY() - start.y);
-                } else if (mode == ZOOM) {
-                    // pinch zooming
-                    float newDist = spacing(event);
-                    Log.d(TAG, "newDist=" + newDist);
-                    if (newDist > 5f) {
-                        matrix.set(savedMatrix);
-                        scale = newDist / oldDist;
-                        // matrix...
-                        // if scale > 1 means
-                        // zoom in...
-                        // if scale < 1 means
-                        // zoom out
-                        matrix.postScale(scale, scale, mid.x, mid.y);
-                    }
-                }
-                break;
-        }
-
-        view.setImageMatrix(matrix); // display the transformation on screen
-
-        return true; // indicate event was handled
-    }
-
-    private float spacing(MotionEvent event) {
-        float x = event.getX(0) - event.getX(1);
-        float y = event.getY(0) - event.getY(1);
-        return (float) Math.sqrt(x * x + y * y);
-    }
-
-    private void midPoint(PointF point, MotionEvent event) {
-        float x = event.getX(0) + event.getX(1);
-        float y = event.getY(0) + event.getY(1);
-        point.set(x / 2, y / 2);
-    }
-
-    private void dumpEvent(MotionEvent event) {
-//        String names[] = { "DOWN", "UP", "MOVE", "CANCEL", "OUTSIDE","POINTER_DOWN", "POINTER_UP", "7?", "8?", "9?" };
-//        StringBuilder sb = new StringBuilder();
-//        int action = event.getAction();
-//        int actionCode = action & MotionEvent.ACTION_MASK;
-//        sb.append("event ACTION_").append(names[actionCode]);
-//
-//        if (actionCode == MotionEvent.ACTION_POINTER_DOWN || actionCode == MotionEvent.ACTION_POINTER_UP)
-//        {
-//            sb.append("(pid ").append(action >> MotionEvent.ACTION_POINTER_ID_SHIFT);
-//            sb.append(")");
-//        }
-//
-//        sb.append("[");
-//        for (int i = 0; i < event.getPointerCount(); i++)
-//        {
-//            sb.append("#").append(i);
-//            sb.append("(pid ").append(event.getPointerId(i));
-//            sb.append(")=").append((int) event.getX(i));
-//            sb.append(",").append((int) event.getY(i));
-//            if (i + 1 < event.getPointerCount())
-//                sb.append(";");
-//        }
-//
-//        sb.append("]");
-//        Log.d("Touch Events ---------", sb.toString());
+    public boolean onTouch(View v, MotionEvent event){
+        zoomLayout.init(PlanetActivity.this);
+        return false;
     }
 }
